9e410c84aa1b8e3d4ef02aaef164691caf7c9a32
satoseiya <s.seiya1014@gmail.com>
2016-06-04 20:13:35 +0900

    Add end point to carrierwave.rb as test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,6 +7,7 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => 'AKIAJUGD52OPF7PV7CIA',
     :aws_secret_access_key  => '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',
     :region => 'ap-northeast-1'
+    :s3_endpoint => 'snapchatboardsbucket.s3-website-ap-northeast-1.amazonaws.com'
   }
   config.fog_directory  = 'snapchatboardsbucket'
 end
